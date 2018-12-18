6fc82c2fbdce170c12bdfe9e400a880c6523c3b7
BulldogX <qiu90123@163.com>
2018-12-18 16:45:45 +0800

    自定义传参

--- ios/RNTest/ViewController.m
+++ ios/RNTest/ViewController.m
@@ -13,6 +13,7 @@
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *rnButton;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 
 @end
 
@@ -37,8 +38,18 @@
 }
 
 - (IBAction)goToRNViewController:(id)sender {
+    
+    //获取textField中的文字
+    NSDictionary *params = nil;
+    
+    if ([self.textField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]].length > 0){
+        params = @{@"msg":[NSString stringWithFormat:@"iOS端传递数据：%@",self.textField.text]};
+    }else{
+        params = @{@"msg":@"我是iOS端传递过来的数据"};
+    }
+    
     NSURL *jsBundlePath = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-    RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsBundlePath moduleName:@"RNTest" initialProperties:nil launchOptions:@{@"msg":@"我是iOS端传递过来的数据"}];
+    RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsBundlePath moduleName:@"RNTest" initialProperties:nil launchOptions:params];
     UIViewController *vc = [[UIViewController alloc] init];
     vc.view = rootView;
     [self.navigationController pushViewController:vc animated:YES];
